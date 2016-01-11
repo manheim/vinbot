3eef6ff5ce467004808defbfc570f21baba12d97
uchagani <umair.chagani@manheim.com>
2016-01-11 16:01:21 -0500

    remove singualr color from vehicle object and return everything we have

--- lib/vinbot/vehicle.rb
+++ lib/vinbot/vehicle.rb
@@ -2,7 +2,7 @@ module Vinbot
   class Vehicle
 
     attr_accessor :year, :make, :model, :trim, :engine_type, :transmission, :body_type, :vehicle_type, :drivetrain,
-                  :interior_colors, :interior_color, :exterior_colors, :exterior_color, :vin
+                  :interior_colors, :exterior_colors, :vin
 
     def initialize
       vehicle = ::Vinbot::Data::Vehicle.order(Sequel.lit('RANDOM()')).limit(1).first
@@ -16,9 +16,7 @@ module Vinbot
       @vehicle_type = vehicle.vehicle_type.name
       @drivetrain = vehicle.drivetrain.name
       @interior_colors = vehicle.interior_color.name
-      @interior_color = @interior_colors.empty? ? '' : @interior_colors.split(',').sample
       @exterior_colors = vehicle.exterior_color.name
-      @exterior_color = @exterior_colors.empty? ? '' : @exterior_colors.split(',').sample
       @vin = Vin.generate_from_squish_vin(vehicle.squish_vin)
     end
 
