cdf54b7475c6b06dd7523718062b345590812ade
Jarod Adair <jarod.adair@manheim.com>
2016-01-14 16:50:19 -0600

    allow year make model params

--- lib/vinbot/vehicle.rb
+++ lib/vinbot/vehicle.rb
@@ -4,8 +4,10 @@ module Vinbot
     attr_accessor :year, :make, :model, :trim, :engine_type, :transmission, :body_type, :vehicle_type, :drivetrain,
                   :interior_colors, :exterior_colors, :vin
 
-    def initialize
-      vehicle = ::Vinbot::Data::Vehicle.order(Sequel.lit('RANDOM()')).limit(1).first
+    def initialize(params = {})
+      params_ids = parse_params(params)
+      binding.pry
+      vehicle = ::Vinbot::Data::Vehicle.where(params_ids).order(Sequel.lit('RANDOM()')).limit(1).first
       @year = vehicle.year.name
       @make = vehicle.make.name
       @model = vehicle.model.name
@@ -18,6 +20,28 @@ module Vinbot
       @interior_colors = vehicle.interior_color.name.split(',')
       @exterior_colors = vehicle.exterior_color.name.split(',')
       @vin = Vin.generate_from_squish_vin(vehicle.squish_vin)
+    end
+
+    private
+
+    def parse_params(params)
+      params_ids = {}
+      params_ids[:year_id] = year_id(params[:year]) if params[:year]
+      params_ids[:make_id] = make_id(params[:make]) if params[:make]
+      params_ids[:model_id] = year_id(params[:model]) if params[:model]
+      params_ids
+    end
+
+    def year_id(year)
+      ::Vinbot::Data::Year.first(name: year).id
+    end
+
+    def make_id(make)
+      ::Vinbot::Data::Make.first(name: make).id
+    end
+
+    def model_id(model)
+      ::Vinbot::Data::Year.first(name: model).id
     end
 
   end
